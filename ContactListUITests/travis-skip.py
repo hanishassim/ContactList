6af231f07265d70779b46f766c17155846506dbf
Hanis <hanis@outlook.com>
2020-05-08 01:14:09 +0800

    Added travis-skip.py to skip UI tests for TravisCI

---
+++ ContactListUITests/travis-skip.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2.7
+
+#
+# travis-skip.py
+# ContactListUITests
+#
+# Skip the UI tests in Travis builds because they aren't supported.
+#
+# We can't run the tests on Travis because Xcode needs permission to use the Accessibility API
+# to control ContactList.
+#
+# See https://github.com/travis-ci/travis-ci/issues/5819
+#
+#
+
+import xml.etree.ElementTree as ET
+
+SCHEME_FILE = "ContactList/ContactList.xcworkspace/xcshareddata/xcschemes/ContactList.xcscheme"
+UI_REF_XPATH = ".//BuildableReference[@BlueprintName='ContactListUITests']/.."
+
+# Parse the Xcode scheme.
+tree = ET.parse(SCHEME_FILE)
+
+# Set the TestableReference for the UI tests to skipped.
+tree.getroot().findall(UI_REF_XPATH)[0].set("skipped", "YES")
+
+# Save the scheme.
+tree.write(SCHEME_FILE)
+
+
